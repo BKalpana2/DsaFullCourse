3a193bb64fded61b2c179127ff0ca346c9abeba0
Kalpana <kalpana.b2714@gmail.com>
2026-09-07 16:13:35 +0530

    basic programs .

--- src/com/basicsprograms/Movingzeroes.java
+++ src/com/basicsprograms/Movingzeroes.java
@@ -3,6 +3,7 @@ package com.basicsprograms;
 public class Movingzeroes {
 public static void main(String[] args) {
 	int arr[]= {0,1,0,3,12};
+	
 	int index=0;//updating non zero elements
 	for(int i=0;i<arr.length;i++) {
 		if(arr[i]!=0) {
--- src/com/basicsprograms/RotateArrayRight.java
+++ src/com/basicsprograms/RotateArrayRight.java
@@ -4,12 +4,11 @@ public class RotateArrayRight {
 
 	public static void main(String[] args) {
 		int arr[]= {10,20,30,40,50};
-		System.out.println(arr.length);
-		System.out.println(arr.length-1);
 		System.out.println("Before Rotate : ");
 		for(int i:arr) {
 			System.out.print(i+" ");
 		}
+		
 		for(int k=1;k<=2;k++) {
 			int first=arr[arr.length-1];
 			for(int i=arr.length-1;i>0;i--) {
